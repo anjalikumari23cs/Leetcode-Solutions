aa0d2717cfc2663d23fe9d31463ab3e3c24a0b89
anjalikumari23cs <anjalikumari93680@gmail.com>
2026-04-27 15:07:26 +0530

    Merge sorted array

---
+++ MergeSortedArray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1; // Pointer for nums1
+        int j = n - 1; // Pointer for nums2
+        int k = m + n - 1; // Pointer for merged array
+        // Merge in reverse order
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        // Copy remaining elements of nums2, if any
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+        return nums1;
+    }
+}
